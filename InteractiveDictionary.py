a4db6bc50eef65005dda8a9da704e4d8702350ba
Sergii-g90 <guk.sergii90@gmail.com>
2018-08-07 11:38:34 +0300

    Adding function to search the word definition

--- InteractiveDictionary.py
+++ InteractiveDictionary.py
@@ -2,3 +2,15 @@ import json
 
 #loading data from json file to 'data' variable (dictionary type)
 data = json.load(open("Data/data.json"))
+
+#Search for the word function
+def search_word (dictionary):
+    word = input("Enter the search word: ")
+    if word in dictionary:
+        #returns description of the found word
+        print("Meaning of the word: ", dictionary[word])
+    else:
+        print("There is no such word")
+
+while 1 == 1:
+    search_word(data)
