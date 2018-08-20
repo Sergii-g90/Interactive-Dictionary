666beb9b4b65065f7aaf8b2a7a2f86821728a586
Sergii-g90 <guk.sergii90@gmail.com>
2018-08-20 14:49:58 +0300

    Adding comment

--- InteractiveDictionary.py
+++ InteractiveDictionary.py
@@ -12,6 +12,7 @@ def search_word (dictionary):
         #returns description of the found word
         print("Meaning of the word: ", dictionary[word])
     else:
+        #if word is not found we are checking for matching words, in case there was mistake in the word
         list_of_matches = difflib.get_close_matches(word, dictionary.keys())
         if list_of_matches != [] :
             print("Meaning of the word", list_of_matches[0], dictionary[list_of_matches[0]])
